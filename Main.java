29a17b9d4c615e82ba9307b09d4bb24ce4494bae
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2022-07-15 15:17:12 -0300

    Variáveis, Tipos de Dados e Operadores Matemáticos em Java
    VTO

---
+++ Main.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int i;
+        //int i;
+        int I;
+        //int 1;
+        //int _1a;
+        //int $aq;
+
+
+        //i = 5;
+       // I = 10;
+       // _1a = 20;
+       // $aq = 7;
+
+       // final int i =10;
+
+
+    }
+
+
+}
