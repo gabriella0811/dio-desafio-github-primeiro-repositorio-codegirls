6279f38326501e408aa387a57567d362f893b3f5
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2022-07-17 12:02:31 -0300

    Trabalhando com Collections Java
    Trabalhando com Collections Java

---
+++ Collections Java/src/br/com/dio/collection/list/ComparatorNomeCorIdade.java
@@ -0,0 +1,9 @@
+package br.com.dio.collection.list;
+
+public class ComparatorNomeCorIdade implements java.util.Comparator<? super Gato> {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
---
+++ Collections Java/src/br/com/dio/collection/list/ComparatorNomeCorIdadeTest.java
@@ -0,0 +1,7 @@
+package br.com.dio.collection.list;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComparatorNomeCorIdadeTest {
+
+}
