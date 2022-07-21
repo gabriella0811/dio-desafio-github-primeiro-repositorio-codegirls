3a50087d2c905cb443c83dffbb67dda4611b8435
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2022-07-21 20:33:34 -0300

    Desafios Básicos Java - Code Girls Santander
    Desafios Básicos Java - Code Girls Santander

---
+++ Desafios Básicos Java - Code Girls Santander/Domino.java
@@ -0,0 +1,14 @@
+package DesafiosJava;
+
+import java.util.Scanner;
+
+public class Domino {
+
+	public static void main(String[] args) {
+		Scanner leitor  = new Scanner(System.in);
+	  int n = leitor.nextInt();
+
+   System.out.println((n+1) * (n+2) / 2);
+   
+   }
+}
