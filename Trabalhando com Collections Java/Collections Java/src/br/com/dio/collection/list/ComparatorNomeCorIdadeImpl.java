882725478438df75af872d80c0595258b90ac1f5
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2022-07-21 20:30:55 -0300

    Trabalhando com Collections Java
    Trabalhando com Collections Java

---
+++ Trabalhando com Collections Java/Collections Java/src/br/com/dio/collection/list/ComparatorNomeCorIdadeImpl.java
@@ -0,0 +1,4 @@
+package br.com.dio.collection.list;
+
+public class ComparatorNomeCorIdadeImpl extends ComparatorNomeCorIdade {
+}
---
+++ Trabalhando com Collections Java/Collections Java/src/br/com/dio/collection/list/List2.java
@@ -0,0 +1,110 @@
+package br.com.dio.collection.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.awt.AWTEventMulticaster.add;
+
+public class List2 {
+
+    public static void main(String[] args) {
+
+        List<Gato> meusGatos = Collections.unmodifiableList(new ArrayList() {
+            void add(new Gato("Jon", 12,"preto") {
+
+            }
+
+            void add(new Gato("Simba", 6,"tigrado") {
+
+            }
+
+            void add(new Gato("Jon", 18,"amarelo") {
+
+            }
+        });
+        meusGatos.sort(Comparator.comparing(Gato::getNome));
+
+
+        System.out.println("--\tOrdem de Inserção\t---");
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem aleatória\t---");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Natural (Nome)\t---");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Idade\t---");
+//        Collections.sort(meusGatos, new ComparatorIdade());
+        meusGatos.sort(new ComparatorIdade());
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem cor\t---");
+//        Collections.sort(meusGatos, new ComparatorCor());
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Nome/Cor/Idade\t---");
+//        Collections.sort(meusGatos, new ComparatorNomeCorIdade());
+        meusGatos.sort(new ComparatorNomeCorIdade());
+        System.out.println(meusGatos);
+    }
+}
+
+class Gato implements Comparable<Gato>{
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Gato gato) {
+        return this.getNome().compareToIgnoreCase(gato.getNome());
+    }
+}
+
+class ComparatorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return Integer.compare(g1.getIdade(), g2.getIdade());
+    }
+}
+
+class ComparatorCor implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+
+}
